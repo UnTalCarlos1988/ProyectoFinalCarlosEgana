6e7e1ec1f0ec5fcbe32613a67457ee4089cfae31
carlos egana <carlos.egana.gutierrez@gmail.com>
2023-11-13 23:37:11 -0300

    problema navbar

--- proyecto1/src/components/NavBar/NavBar.jsx
+++ proyecto1/src/components/NavBar/NavBar.jsx
@@ -2,14 +2,14 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import CartWidget from '../CartWidget/CartWidget';
-import logo from '../NavBar/logo';
+import Logo from '../NavBar/logo';
 
 function NavBar() {
   return (
     <>
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">  <logo/>  </Navbar.Brand>
+          <Navbar.Brand href="#home">  <Logo/>  </Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">proteinas</Nav.Link>
@@ -24,4 +24,4 @@ function NavBar() {
       );
 }
 
-export default ColorSchemesExample;
+export default NavBar;
--- proyecto1/src/components/NavBar/logo.jsx
+++ proyecto1/src/components/NavBar/logo.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const logo = () =>{
+const Logo = () =>{
     return (
         <div>
-            <img src="https://www.pngwing.com/es/free-png-kwfod" width="50px" height="50px" alt="logo" />
+            <img src="https://images.app.goo.gl/1BgqZnGucQ2nN8KWA" width="50px" height="50px" alt="logo" />
         </div>
     )
 }
 
-export default logo
+export default Logo
