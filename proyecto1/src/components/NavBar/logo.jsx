35d0330755b85af300bb6ceef0d88d8346677846
carlos egana <carlos.egana.gutierrez@gmail.com>
2023-12-09 12:26:31 -0300

    solucion a logo

--- proyecto1/src/components/NavBar/logo.jsx
+++ proyecto1/src/components/NavBar/logo.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import logosuple from "../NavBar/logosuple.png";
 
-const Logo = () =>{
-    return (
-        <div>
-            <img src="https://www.flaticon.es/icono-gratis/suplementos-de-esteroides-para-gimnastas-para-ejercicios-con-mancuernas_30817" width="50px" height="50px" alt="logo" />
-        </div>
-    )
-}
+const Logo = () => {
+  return (
+    <div>
+      <img src={logosuple} width="50px" height="50px" alt="logo" />
+    </div>
+  );
+};
 
-export default Logo
+export default Logo;
